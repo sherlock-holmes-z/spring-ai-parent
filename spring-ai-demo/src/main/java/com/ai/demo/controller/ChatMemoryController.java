f70d120b713344e89c1dcbadb1d859b2c2e47bb2
gulu <1105026730@qq.com>
2026-06-10 01:33:26 +0800

    ci(build): 会话记忆

--- spring-ai-demo/src/main/java/com/ai/demo/controller/ChatMemoryController.java
+++ spring-ai-demo/src/main/java/com/ai/demo/controller/ChatMemoryController.java
@@ -4,6 +4,9 @@ import com.ai.demo.advisor.SimpleChatMemoryAdvisor;
 import com.ai.demo.advisor.SimpleChatMemoryContextAdvisor;
 import lombok.AllArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatMemoryController {
 
     private final ChatClient chatClient;
+
+    // 创建一个自动上下文Advisor,MessageWindowChatMemory使用内存记忆
+    static MessageWindowChatMemory chatMemory = MessageWindowChatMemory.builder().maxMessages(100).build();
+    static MessageChatMemoryAdvisor chatMemoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
 
 
     @GetMapping("/chat")
@@ -36,6 +43,23 @@ public class ChatMemoryController {
                 .advisors(advisorSpec -> {
                     advisorSpec.advisors(new SimpleChatMemoryContextAdvisor());
                     advisorSpec.param("conversationId", conversationId);
+                })
+                .call()
+                .content();
+    }
+
+
+    @GetMapping("/chat/auto/context")
+    public String chatWithAutoContext(@RequestParam(name = "message") String  message,
+                                      @RequestParam(name = "conversationId") String  conversationId) {
+
+        // 添加自动上下文
+        return chatClient.prompt()
+                .system("你是聊天助手")
+                .user(message)
+                .advisors(advisorSpec -> {
+                    advisorSpec.advisors(chatMemoryAdvisor);
+                    advisorSpec.param(ChatMemory.CONVERSATION_ID, conversationId);
                 })
                 .call()
                 .content();
